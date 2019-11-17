50994c75163c5b4cb7deb2f0b858313a4b94f103
erwinleonardy <erwin.leonardy30@gmail.com>
2019-11-17 15:42:03 +0700

    Fixed os.environ.get bug

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, jsonify, request
 from models import *
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
